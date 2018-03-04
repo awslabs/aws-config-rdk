cd101732b9c9f85e72541dd43030040b2364982a
Michael Borchert <mborch@amazon.com>
2018-03-04 08:05:23 +0800

    Fixed build id regex.

--- testing/copy_to_s3.sh
+++ testing/copy_to_s3.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-
-if [[ $CODEBUILD_SOURCE_VERSION =~ "MyApp\/(.*?).zip" ]]; then
+#MyApp\/(.*?).zip
+echo $CODEBUILD_SOURCE_VERSION
+if [[ $CODEBUILD_SOURCE_VERSION =~ MyApp\/(.*).zip ]]; then
+  echo ${BASH_REMATCH[1]}``;
   aws s3 sync . s3://rdk-testing-source-bucket/${BASH_REMATCH[1]}/;
-else
-  echo $CODEBUILD_SOURCE_VERSION;
 fi
