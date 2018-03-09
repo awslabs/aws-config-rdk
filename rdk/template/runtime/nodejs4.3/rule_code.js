a14125462f6413fa20b46bcb51b206935fd2906b
Michael Borchert <mborch@amazon.com>
2018-03-09 22:10:07 +0800

    Updating nodejs runtime to fix bugs.

--- rdk/template/runtime/nodejs4.3/rule_code.js
+++ rdk/template/runtime/nodejs4.3/rule_code.js
@@ -8,35 +8,18 @@
 #    or in the "license" file accompanying this file. This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
 */
 
-// RULE DESCRIPTION
-/// This example rule checks that EC2 instances are of the desired instance type
-// The desired instance type is specified in the rule parameters.
-//
-// RULE DETAILS
-// Trigger Type (Change Triggered or Periodic: Change Triggered
-
-// Required Parameters: desiredInstanceType - t2.micro
-// Rule parameters defined in rules/ruleCode/ruleParameters.txt
-
 'use strict';
 
 const aws = require('aws-sdk');
 
 const config = new aws.ConfigService();
-// This rule needs to be uploaded with rule_util.py. It is automatically done when using the RDK.
-const rule_util = require('./rule_util');
 
 // This is where it's determined whether the resource is compliant or not.
 // In this example, we simply decide that the resource is compliant if it is an instance and its type matches the type specified as the desired type.
 // If the resource is not an EC2 instance, then we deem this resource to be not applicable. (If the scope of the rule is specified to include only
 // instances, this rule would never have been invoked.)
 function evaluateCompliance(configurationItem, ruleParameters) {
-    if (configurationItem.resourceType !== 'AWS::EC2::Instance') {
-        return 'NOT_APPLICABLE';
-    } else if (ruleParameters.desiredInstanceType === configurationItem.configuration.instanceType) {
-        return 'COMPLIANT';
-    }
-    return 'NON_COMPLIANT';
+    return 'NOT_APPLICABLE';
 }
 
 function rule_handler(event, context, callback) {
@@ -45,10 +28,6 @@ function rule_handler(event, context, callback) {
     const configItem = invokingEvent.configurationItem;
     const ruleParameters = JSON.parse(event.ruleParameters);
     callback(null, evaluateCompliance(configItem, ruleParameters));
-}
-
-exports.lambda_handler = (event, context, callback) => {
-    rule_util.decorate_handler(rule_handler)(event, context, callback);
 }
 
 // Helper function used to validate input
@@ -129,7 +108,7 @@ function isApplicable(configurationItem, event) {
 
 // This is the handler that's invoked by Lambda
 // Most of this code is boilerplate; use as is
-exports.lambda_handler = (event, context, callback) => {
+exports.lambda_handler = function(event, context, callback) {
     checkDefined(event, 'event');
     const invokingEvent = JSON.parse(event.invokingEvent);
     const ruleParameters = JSON.parse(event.ruleParameters);
@@ -160,17 +139,17 @@ exports.lambda_handler = (event, context, callback) => {
                   ComplianceResourceId: configurationItem.resourceId,
                   ComplianceType: compliance,
                   OrderingTimestamp: configurationItem.configurationItemCaptureTime,
-              },
+              }
           ];
-        } else if (compliance instanceof Array){
-          fields = ['ComplianceResourceType', 'ComplianceResourceId', 'ComplianceType', 'OrderingTimestamp']
+        } else if (compliance instanceof Array) {
+          fields = ['ComplianceResourceType', 'ComplianceResourceId', 'ComplianceType', 'OrderingTimestamp'];
           for (var i = 0; i < compliace.length; i++) {
             compliance_result = compliance[i];
 
             var missing_fields = false;
-            for (var j = 0; j < fields.length){
-              if (!compliance_result[fields[j]){
-                console.info("Missing " + fields[j] + " from custom evaluation.")
+            for (var j = 0; j < fields.length; j++) {
+              if (!compliance_result.hasOwnProperty(fields[j])) {
+                console.info("Missing " + fields[j] + " from custom evaluation.");
                 missing_fields = true;
               }
             }
@@ -186,7 +165,7 @@ exports.lambda_handler = (event, context, callback) => {
                   ComplianceResourceId: configurationItem.resourceId,
                   ComplianceType: 'NOT_APPLICABLE',
                   OrderingTimestamp: configurationItem.configurationItemCaptureTime,
-              },
+              }
           ];
         }
 
