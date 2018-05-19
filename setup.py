8cbfd8ec68fbc8842b96507cc8ad350ce1c7120a
Michael Borchert <mborch@amazon.com>
2018-05-19 23:23:17 +0800

    Bumping version for pypi upload.

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def readme():
         return f.read()
 
 setup(name='rdk',
-      version='0.3.12',
+      version='0.3.13',
       description='Rule Development Kit CLI for AWS Config',
       long_description=readme(),
       url='https://github.com/awslabs/aws-config-rdk/',
