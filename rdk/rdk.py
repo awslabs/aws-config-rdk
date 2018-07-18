70898e5f5271038c1d47df57077233fa9b40b2ee
Michael Borchert <mborch@amazon.com>
2018-07-18 11:33:19 +0800

    Added error-handling to TestCI in case CI is valid, but we don't have a sample for it.

--- rdk/rdk.py
+++ rdk/rdk.py
@@ -1493,7 +1493,11 @@ class TestCI():
     def __init__(self, ci_type):
         #convert ci_type string to filename format
         ci_file = ci_type.replace('::','_') + '.json'
-        self.ci_json = json.load(open(os.path.join(path.dirname(__file__), 'template',  example_ci_dir, ci_file), 'r'))
-
+        try:
+            self.ci_json = json.load(open(os.path.join(path.dirname(__file__), 'template',  example_ci_dir, ci_file), 'r'))
+        except FileNotFoundError:
+            print("No sample CI found for " + ci_type + ", even though it appears to be a supported CI.  Please log an issue at https://github.com/awslabs/aws-config-rdk.")
+            exit(1)
+            
     def get_json(self):
         return self.ci_json
