5cec78260291586063b69879d13cb0bab6be2885
rafih <rafih@amazon.com>
2018-07-28 14:58:47 +0530

    display rdk version when -v/--version specified and formatting for rulesets list

--- rdk/__init__.py
+++ rdk/__init__.py
@@ -5,3 +5,6 @@
 #        http://aws.amazon.com/apache2.0/
 #
 #    or in the "license" file accompanying this file. This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
+
+
+__version__ = "0.3.20"
