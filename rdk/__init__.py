8728db56509868c725a0b2fdd25153f63dacd5eb
Michael Borchert <mborch@amazon.com>
2018-09-07 10:51:05 +0800

    Bumping version for bugfix update.

--- rdk/__init__.py
+++ rdk/__init__.py
@@ -6,4 +6,4 @@
 #
 #    or in the "license" file accompanying this file. This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
 
-MY_VERSION = "0.0.30"
+MY_VERSION = "0.0.31"
