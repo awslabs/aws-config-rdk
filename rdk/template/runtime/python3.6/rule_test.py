e7665d6e3a6e2c1583ae767f830fbc37c44f9bd8
jrault <jrault@git.amazon.com>
2019-03-06 09:18:03 +0800

    improve style + import

--- rdk/template/runtime/python3.6/rule_test.py
+++ rdk/template/runtime/python3.6/rule_test.py
@@ -1,10 +1,10 @@
 import sys
 import unittest
 try:
-    from unittest.mock import MagicMock, patch, ANY
+    from unittest.mock import MagicMock
 except ImportError:
     import mock
-    from mock import MagicMock, patch, ANY
+    from mock import MagicMock
 import botocore
 from botocore.exceptions import ClientError
 
@@ -19,21 +19,21 @@ DEFAULT_RESOURCE_TYPE = 'AWS::::Account'
 # Main Code #
 #############
 
-config_client_mock = MagicMock()
-sts_client_mock = MagicMock()
+CONFIG_CLIENT_MOCK = MagicMock()
+STS_CLIENT_MOCK = MagicMock()
 
 class Boto3Mock():
     def client(self, client_name, *args, **kwargs):
         if client_name == 'config':
-            return config_client_mock
+            return CONFIG_CLIENT_MOCK
         elif client_name == 'sts':
-            return sts_client_mock
+            return STS_CLIENT_MOCK
         else:
             raise Exception("Attempting to create an unknown client")
 
 sys.modules['boto3'] = Boto3Mock()
 
-rule = __import__('<%RuleName%>')
+RULE = __import__('<%RuleName%>')
 
 class SampleTest(unittest.TestCase):
 
@@ -48,8 +48,8 @@ class SampleTest(unittest.TestCase):
         self.assertTrue(True)
 
     #def test_sample_2(self):
-    #    rule.ASSUME_ROLE_MODE = False
-    #    response = rule.lambda_handler(build_lambda_configurationchange_event(self.invoking_event_iam_role_sample, self.rule_parameters), {})
+    #    RULE.ASSUME_ROLE_MODE = False
+    #    response = RULE.lambda_handler(build_lambda_configurationchange_event(self.invoking_event_iam_role_sample, self.rule_parameters), {})
     #    resp_expected = []
     #    resp_expected.append(build_expected_response('NOT_APPLICABLE', 'some-resource-id', 'AWS::IAM::Role'))
     #    assert_successful_evaluation(self, response, resp_expected)
@@ -101,35 +101,35 @@ def build_expected_response(compliance_type, compliance_resource_id, compliance_
         'Annotation': annotation
         }
 
-def assert_successful_evaluation(testClass, response, resp_expected, evaluations_count=1):
+def assert_successful_evaluation(test_class, response, resp_expected, evaluations_count=1):
     if isinstance(response, dict):
-        testClass.assertEquals(resp_expected['ComplianceResourceType'], response['ComplianceResourceType'])
-        testClass.assertEquals(resp_expected['ComplianceResourceId'], response['ComplianceResourceId'])
-        testClass.assertEquals(resp_expected['ComplianceType'], response['ComplianceType'])
-        testClass.assertTrue(response['OrderingTimestamp'])
+        test_class.assertEquals(resp_expected['ComplianceResourceType'], response['ComplianceResourceType'])
+        test_class.assertEquals(resp_expected['ComplianceResourceId'], response['ComplianceResourceId'])
+        test_class.assertEquals(resp_expected['ComplianceType'], response['ComplianceType'])
+        test_class.assertTrue(response['OrderingTimestamp'])
         if 'Annotation' in resp_expected or 'Annotation' in response:
-            testClass.assertEquals(resp_expected['Annotation'], response['Annotation'])
+            test_class.assertEquals(resp_expected['Annotation'], response['Annotation'])
     elif isinstance(response, list):
-        testClass.assertEquals(evaluations_count, len(response))
+        test_class.assertEquals(evaluations_count, len(response))
         for i, response_expected in enumerate(resp_expected):
-            testClass.assertEquals(response_expected['ComplianceResourceType'], response[i]['ComplianceResourceType'])
-            testClass.assertEquals(response_expected['ComplianceResourceId'], response[i]['ComplianceResourceId'])
-            testClass.assertEquals(response_expected['ComplianceType'], response[i]['ComplianceType'])
-            testClass.assertTrue(response[i]['OrderingTimestamp'])
+            test_class.assertEquals(response_expected['ComplianceResourceType'], response[i]['ComplianceResourceType'])
+            test_class.assertEquals(response_expected['ComplianceResourceId'], response[i]['ComplianceResourceId'])
+            test_class.assertEquals(response_expected['ComplianceType'], response[i]['ComplianceType'])
+            test_class.assertTrue(response[i]['OrderingTimestamp'])
             if 'Annotation' in response_expected or 'Annotation' in response[i]:
-                testClass.assertEquals(response_expected['Annotation'], response[i]['Annotation'])
+                test_class.assertEquals(response_expected['Annotation'], response[i]['Annotation'])
 
-def assert_customer_error_response(testClass, response, customerErrorCode=None, customerErrorMessage=None):
-    if customerErrorCode:
-        testClass.assertEqual(customerErrorCode, response['customerErrorCode'])
-    if customerErrorMessage:
-        testClass.assertEqual(customerErrorMessage, response['customerErrorMessage'])
-    testClass.assertTrue(response['customerErrorCode'])
-    testClass.assertTrue(response['customerErrorMessage'])
+def assert_customer_error_response(test_class, response, customer_error_code=None, customer_error_message=None):
+    if customer_error_code:
+        test_class.assertEqual(customer_error_code, response['customerErrorCode'])
+    if customer_error_message:
+        test_class.assertEqual(customer_error_message, response['customerErrorMessage'])
+    test_class.assertTrue(response['customerErrorCode'])
+    test_class.assertTrue(response['customerErrorMessage'])
     if "internalErrorMessage" in response:
-        testClass.assertTrue(response['internalErrorMessage'])
+        test_class.assertTrue(response['internalErrorMessage'])
     if "internalErrorDetails" in response:
-        testClass.assertTrue(response['internalErrorDetails'])
+        test_class.assertTrue(response['internalErrorDetails'])
 
 def sts_mock():
     assume_role_response = {
@@ -137,8 +137,8 @@ def sts_mock():
             "AccessKeyId": "string",
             "SecretAccessKey": "string",
             "SessionToken": "string"}}
-    sts_client_mock.reset_mock(return_value=True)
-    sts_client_mock.assume_role = MagicMock(return_value=assume_role_response)
+    STS_CLIENT_MOCK.reset_mock(return_value=True)
+    STS_CLIENT_MOCK.assume_role = MagicMock(return_value=assume_role_response)
 
 ##################
 # Common Testing #
@@ -147,17 +147,17 @@ def sts_mock():
 class TestStsErrors(unittest.TestCase):
 
     def test_sts_unknown_error(self):
-        rule.ASSUME_ROLE_MODE = True
-        sts_client_mock.assume_role = MagicMock(side_effect=botocore.exceptions.ClientError(
+        RULE.ASSUME_ROLE_MODE = True
+        STS_CLIENT_MOCK.assume_role = MagicMock(side_effect=botocore.exceptions.ClientError(
             {'Error': {'Code': 'unknown-code', 'Message': 'unknown-message'}}, 'operation'))
-        response = rule.lambda_handler(build_lambda_configurationchange_event('{}'), {})
+        response = RULE.lambda_handler(build_lambda_configurationchange_event('{}'), {})
         assert_customer_error_response(
             self, response, 'InternalError', 'InternalError')
 
     def test_sts_access_denied(self):
-        rule.ASSUME_ROLE_MODE = True
-        sts_client_mock.assume_role = MagicMock(side_effect=botocore.exceptions.ClientError(
+        RULE.ASSUME_ROLE_MODE = True
+        STS_CLIENT_MOCK.assume_role = MagicMock(side_effect=botocore.exceptions.ClientError(
             {'Error': {'Code': 'AccessDenied', 'Message': 'access-denied'}}, 'operation'))
-        response = rule.lambda_handler(build_lambda_configurationchange_event('{}'), {})
+        response = RULE.lambda_handler(build_lambda_configurationchange_event('{}'), {})
         assert_customer_error_response(
             self, response, 'AccessDenied', 'AWS Config does not have permission to assume the IAM role.')
