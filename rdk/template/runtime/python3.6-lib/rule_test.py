86f689ae5288910d1578be89016eeb9d4be97f70
shikhar <shikharj@amazon.com>
2019-04-23 09:11:39 +0000

    updating template to match 3.6

--- rdk/template/runtime/python3.6-lib/rule_test.py
+++ rdk/template/runtime/python3.6-lib/rule_test.py
@@ -26,10 +26,9 @@ STS_CLIENT_MOCK = MagicMock()
 def mock_get_client(client_name, *args, **kwargs):
     if client_name == 'config':
         return CONFIG_CLIENT_MOCK
-    elif client_name == 'sts':
+    if client_name == 'sts':
         return STS_CLIENT_MOCK
-    else:
-        raise Exception("Attempting to create an unknown client")
+    raise Exception("Attempting to create an unknown client")
 
 MODULE = __import__('<%RuleName%>')
 RULE = MODULE.<%RuleName%>()
@@ -52,34 +51,3 @@ class SampleTest(unittest.TestCase):
     #    resp_expected = []
     #    resp_expected.append(build_expected_response('NOT_APPLICABLE', 'some-resource-id', 'AWS::IAM::Role'))
     #    assert_successful_evaluation(self, response, resp_expected)
-
-def sts_mock():
-    assume_role_response = {
-        "Credentials": {
-            "AccessKeyId": "string",
-            "SecretAccessKey": "string",
-            "SessionToken": "string"}}
-    STS_CLIENT_MOCK.reset_mock(return_value=True)
-    STS_CLIENT_MOCK.assume_role = MagicMock(return_value=assume_role_response)
-
-##################
-# Common Testing #
-##################
-
-class TestStsErrors(unittest.TestCase):
-
-    @patch('rdklib.rdklib.get_client', side_effect=botocore.exceptions.ClientError(
-        {'Error': {'Code': 'InternalError', 'Message': 'InternalError'}}, 'operation'))
-    def test_sts_unknown_error(self, my_mock):
-        response = MODULE.lambda_handler(rdklib.build_lambda_scheduled_event(), {})
-        print(response)
-        rdklib.assert_customer_error_response(
-            self, response, 'InternalError', 'InternalError')
-
-    @patch('rdklib.rdklib.get_client', side_effect=botocore.exceptions.ClientError(
-        {'Error': {'Code': 'AccessDenied', 'Message': 'AWS Config does not have permission to assume the IAM role.'}}, 'operation'))
-    def test_sts_access_denied(self, my_mock):
-        response = MODULE.lambda_handler(rdklib.build_lambda_scheduled_event(), {})
-        print(response)
-        rdklib.assert_customer_error_response(
-            self, response, 'AccessDenied', 'AWS Config does not have permission to assume the IAM role.')
