236a056b9c0486adbe79560604d853440a30e1a7
Michael Borchert <mborch@amazon.com>
2019-10-17 14:54:49 -0500

    Fixed remediation SSM parameters structure.

--- rdk/rdk.py
+++ rdk/rdk.py
@@ -683,8 +683,9 @@ class rdk:
             self.args.auto_remediate = params.get("Automatic", "")
             execution_controls = params.get("ExecutionControls", "")
             if execution_controls:
-                self.args.remediation_concurrent_execution_percent = execution_controls.get("ConcurrentExecutionRatePercentage", "")
-                self.args.remediation_error_rate_percent = execution_controls.get("ErrorPercentage", "")
+                ssm_controls = execution_controls["SsmControls"]
+                self.args.remediation_concurrent_execution_percent = ssm_controls.get("ConcurrentExecutionRatePercentage", "")
+                self.args.remediation_error_rate_percent = ssm_controls.get("ErrorPercentage", "")
             self.args.remediation_parameters = json.dumps(params.get("Parameters", ""))
             self.args.auto_remediation_retry_time = params.get("RetryAttemptSeconds", "")
             self.args.remediation_action = params.get("TargetId", "")
@@ -2029,7 +2030,7 @@ class rdk:
             ssm_controls["ErrorPercentage"] = self.args.remediation_error_rate_percent
 
         if ssm_controls:
-            params["ExecutionControls"] = ssm_controls
+            params["ExecutionControls"] = {"SsmControls": ssm_controls}
 
         if self.args.auto_remediation_retry_attempts:
             params["MaximumAutomaticAttempts"] = self.args.auto_remediation_retry_attempts
