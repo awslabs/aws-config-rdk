3a32ae2cc07abc9a53cad09d00031d08868bbf27
Jarrett Andrulis <jandruli@amazon.com>
2021-06-09 12:00:25 -0500

    updated partition test

--- testing/partition_test.py
+++ testing/partition_test.py
@@ -1,5 +1,6 @@
 import boto3
 import subprocess
+import sys
 
 testing_regions = {
     "aws": ["us-east-1", "ap-southeast-1", "eu-central-1", "sa-east-1"],
@@ -9,5 +10,18 @@ testing_regions = {
 sts_client = boto3.client("sts")
 partition = sts_client.get_caller_identity()["Arn"].split(":")[1]
 
-for region in testing_regions[partition]:
+subprocesses = [
     subprocess.Popen(["rdk", "-r", region, "init"])
+    for region in testing_regions[partition]
+]
+
+received_bad_return_code = False
+
+for process in subprocesses:
+    process.wait()
+    if process.returncode != 0:
+        print(process.communicate())
+        received_bad_return_code = True
+
+if received_bad_return_code:
+    sys.exit(1)
