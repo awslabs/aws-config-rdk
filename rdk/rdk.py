f210770cee1d07cba2f68b71deffb5157e4fcf07
Jarrett Andrulis <jandruli@amazon.com>
2021-12-15 15:22:42 -0600

    Separated opt-in and default regions

--- rdk/rdk.py
+++ rdk/rdk.py
@@ -2516,7 +2516,7 @@ class rdk:
     def create_region_set(self):
         self.args = get_create_region_set_parser().parse_args(self.args.command_args, self.args)
         output_file = self.args.output_file
-        output_dict = {"default":["us-east-1","us-west-1","eu-north-1","ap-east-1"],"aws-cn-region-set":["cn-north-1","cn-northwest-1"]}
+        output_dict = {"default":["us-east-1","us-west-1","eu-north-1","ap-southeast-1"],"aws-cn-region-set":["cn-north-1","cn-northwest-1"]}
         with open(f"{output_file}.yaml","w+") as file:
             yaml.dump(output_dict, file, default_flow_style = False)
 
