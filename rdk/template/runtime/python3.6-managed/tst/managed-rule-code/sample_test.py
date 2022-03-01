2de33543c946fbd557fec6f565f41c6eadac4999
Jarrett Andrulis <Jarrett9999@gmail.com>
2022-03-01 16:00:03 -0600

    Supported resources (#334)
    * Formatted stuff, added a lot of new sample ci's and added some more info from `rdk --help` and `rdk sample-ci`
    
    Co-authored-by: Jarrett Andrulis <jandruli@amazon.com>

--- rdk/template/runtime/python3.6-managed/tst/managed-rule-code/sample_test.py
+++ rdk/template/runtime/python3.6-managed/tst/managed-rule-code/sample_test.py
@@ -1,8 +1,9 @@
 import unittest
 
+
 class SampleTest(unittest.TestCase):
     def setUp(self):
         pass
-    
+
     def test_sample(self):
-        self.assertTrue(True);
+        self.assertTrue(True)
