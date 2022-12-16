2a1a2ca0b693b7997ae5e7a2e515d49d8e1a48d8
Mark Beacom <7315957+mbeacom@users.noreply.github.com>
2022-12-16 14:11:10 -0500

    Standardize on underscores for TF resource IDs

--- rdk/template/terraform/1.x/config_rule.tf
+++ rdk/template/terraform/1.x/config_rule.tf
@@ -194,7 +194,7 @@ resource "aws_iam_role_policy_attachment" "awsconfig_policy_attach" {
   policy_arn = aws_iam_policy.awsconfig_policy[count.index].arn
 }
 
-resource "aws_iam_role_policy_attachment" "readonly-role-policy-attach" {
+resource "aws_iam_role_policy_attachment" "readonly_role_policy_attach" {
   count      = local.create_new_lambda_role ? 1 : 0
   role       = aws_iam_role.awsconfig[count.index].name
   policy_arn = data.aws_iam_policy.read_only_access.arn
