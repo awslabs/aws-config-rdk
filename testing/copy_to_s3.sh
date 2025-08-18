875075e97388752df0f1bae0b4c0ac8d0547b26a
israel4435-ux <israel4435@gmail.com>
2025-08-18 15:39:39 -0700

    Delete testing directory

--- testing/copy_to_s3.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#MyApp\/(.*?).zip
-echo $CODEBUILD_SOURCE_VERSION
-if [[ $CODEBUILD_SOURCE_VERSION =~ MyApp\/(.*).zip ]]; then
-  echo ${BASH_REMATCH[1]}``;
-  aws s3 sync . s3://rdk-testing-source-bucket/${BASH_REMATCH[1]}/;
-fi
--- testing/test_windows.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-#Params
-#python_version must be "3"
-python_version=$1
-
-echo $CODEBUILD_SOURCE_VERSION
